46c76f4fac17c05bc8df4f10053ec98ca1947cee
Luke <admin@frozenphoenix.com.au>
2025-11-10 01:37:09 +1100

    test

---
+++ Assets/_Scripts/Manager/SoundManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager _instance;
+    
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("SoundManager is null");
+            }
+            return _instance;
+        }
+    }
+    
+    private AudioSource _audioSource;
+    
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        _instance = this;
+        
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public float GetCurrentVolume()
+    {
+        return _audioSource.volume;
+    }
+
+    public bool IsMuted()
+    {
+        return _audioSource.mute;
+    }
+
+    public void ToggleMute(bool mute)
+    {
+        _audioSource.mute = mute;
+    }
+
+    public void AdjustSoundVolume(float volume)
+    {
+        _audioSource.volume = volume;
+    }
+}
---
+++ Assets/_Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private UIDocument _uiDocument;
+    
+    private Button _playButton;
+    private Button _settingsButton;
+    private Button _creditsButton;
+
+    private void Awake()
+    {
+        _uiDocument = GetComponent<UIDocument>();
+        if (_uiDocument == null)
+        {
+            Debug.LogError("No UIDocument found on MainMenuManager.");
+        }
+        
+        _playButton = _uiDocument.rootVisualElement.Q<Button>("Play_Button");
+        _playButton.RegisterCallback<ClickEvent>(LoadGameScene);
+        
+        _settingsButton = _uiDocument.rootVisualElement.Q<Button>("Settings_Button");
+        _settingsButton.RegisterCallback<ClickEvent>(ShowSettingsUI);
+    }
+    
+    private void LoadGameScene(ClickEvent evt)
+    {
+        SceneManager.LoadScene("Game");
+    }
+    
+    private void ShowSettingsUI(ClickEvent evt)
+    {
+        SettingsUIManager.Instance.SetVisible();
+    }
+}
